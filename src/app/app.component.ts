0608e1bb22f509c98124960b36322c89eaaa77f2
RomanMaiboroda <roman.maiboroda@innovecs.com>
2018-09-10 18:01:24 +0300

    Changes for 10.09.18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,43 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  value = 5;
-
-  serverElements = [
-    {type: 'server', name: 'TestServer', content: 'TestServer Content'}
-    ];
-
-  onSayHiEvent(data: {sayHiData: string}) {
-    console.log(data.sayHiData);
-  }
-
-  onServerAdded(serverData: {serverName: string, serverContent: string}) {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent
-    });
-  }
-
-  onBlueprintAdded(serverData: {serverName: string, serverContent: string}) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: serverData.serverName,
-      content: serverData.serverContent
-    });
-  }
-
-  onServerDeleted(serverData: {serverName: string}) {
-    this.serverElements = this.serverElements.filter( item =>
-      item.name !== serverData.serverName);
-  }
-
-  onBluePrintDeleted(serverData: {serverName: string}) {
-    this.serverElements = this.serverElements.filter( item =>
-      item.name !== serverData.serverName);
-  }
-
 }
