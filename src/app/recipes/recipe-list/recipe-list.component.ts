a4e5f07739d6ca24ea72ac142228c1392f151ca4
RomanMaiboroda <roman.maiboroda@innovecs.com>
2018-09-27 11:14:51 +0300

    Changes for 27.09.18

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../../services/recipe.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,9 +11,14 @@ import {RecipeService} from '../../services/recipe.service';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService,
+              private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.recipesArr;
+  }
+
+  onNewRecipe() {
+    this.router.navigate(['new'], {relativeTo: this.route} );
   }
 }
