77192822f36121bde8e272c9cead8da7b6864f37
RomanMaiboroda <roman.maiboroda@innovecs.com>
2018-10-03 16:36:18 +0300

    changes for 03.10.18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,57 @@ import {NgForm} from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
+  @ViewChild('form') form: NgForm;
+
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    useremail: '',
+    userquestion: '',
+    useranswer: '',
+    usergender: ''
+  }
+  submited = false;
 
   constructor() {}
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    // this.signupForm.setValue({ // Must include all keys: value
+    //   userData: {
+    //     username: suggestedName,
+    //     useremail: ''
+    //   },
+    //   userquestion: 'pet',
+    //   useranswer: '',
+    //   usergender: 'male'
+    // });
+
+    this.signupForm.form.patchValue({ // overwriting part of the form
+      userData: {
+        username: suggestedName
+      }
+    });
+  }
 
   // onSubmit(form: NgForm) {
   //   console.log(form);
   // }
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.useremail = this.signupForm.value.userData.useremail;
+    this.user.userquestion = this.signupForm.value.userquestion;
+    this.user.useranswer = this.signupForm.value.useranswer;
+    this.user.usergender = this.signupForm.value.gender;
+    this.submited = true;
+
+    this.signupForm.reset();
+  }
+
+  onSubmitForm() {
+    console.log(this.form);
   }
 }
