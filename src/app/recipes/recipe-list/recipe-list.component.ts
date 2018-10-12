76aac24949d0f7200d49de0926280b07dabc0d80
RomanMaiboroda <roman.maiboroda@innovecs.com>
2018-10-12 17:32:14 +0300

    changes for 12.10.18

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,24 +1,35 @@
-import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../../services/recipe.service';
 import {ActivatedRoute, Router} from '@angular/router';
+import {Subscription} from 'rxjs/index';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
+  private subscription: Subscription;
 
   constructor(private recipeService: RecipeService,
               private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.recipesArr;
+    this.subscription = this.recipeService.recipeChanged.subscribe(
+      (recipes: Recipe[]) => {
+        this.recipes = recipes;
+      }
+    );
   }
 
   onNewRecipe() {
     this.router.navigate(['new'], {relativeTo: this.route} );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -2,16 +2,19 @@ import {Injectable} from '@angular/core';
 import { Recipe } from '../recipes/recipe.model';
 import {Ingredient} from '../shopping-list/ingredient.model';
 import {ShoppingListService} from './shoping-list.service';
+import {Subject} from 'rxjs/index';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
+  recipeChanged = new Subject<Recipe[]>()
 
   recipesArr: Recipe[] = [
     new Recipe(
       'First Recipe',
       'Desc of First Recipe',
+      'https://images.pexels.com/photos/257840/pexels-photo-257840.jpeg',
       [
         new Ingredient('Meat', 100),
         new Ingredient('Solt', 1)
@@ -19,10 +22,11 @@ export class RecipeService {
     ),
     new Recipe(
       'Second Recipe',
-      'Desc of First Recipe',
+      'Desc of Second Recipe',
+      'http://www.smartweek.it/wpsw/wp-content/uploads/2016/02/Pere.jpg',
       [
-        new Ingredient('Meat', 100),
-        new Ingredient('Solt', 1)
+        new Ingredient('Meat', 20),
+        new Ingredient('Solt', 2)
       ]
     ),
   ];
@@ -33,8 +37,13 @@ export class RecipeService {
     return this.recipesArr[id];
   }
 
-  addRecipe(name: string, desc: string, ingredients: any) {
-    this.recipesArr.push({name, desc, ingredients});
+  addRecipe(recipe: Recipe) {
+    this.recipesArr.push(recipe);
+    this.recipeChanged.next(this.recipesArr.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipesArr[index] = newRecipe;
   }
 
   removeRecipe(name: string) {
